3a5a315b8d47e1016ed901cab602412517f924e1
pmarcel-uw <pmarcel@uw.com>
2023-05-04 22:30:09 -0700

    implemented the persistent drawer to make the app work with smaller screens

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,20 +2,19 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 
 import React, { useState } from 'react';
-import { Link as RouterLink } from 'react-router-dom';
+import { NavLink as RouterLink } from 'react-router-dom';
 import {
     AppBar,
     Toolbar,
     IconButton,
     Typography,
-    Button,
-    Link,
     Box,
     Drawer,
     List,
     ListItem,
     ListItemIcon,
     ListItemText,
+    Avatar,
 } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import HomeIcon from '@mui/icons-material/Home';
@@ -26,9 +25,11 @@ import LockOpenIcon from '@mui/icons-material/LockOpen';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import { useAuth0 } from '@auth0/auth0-react';
 
+const drawerWidth = 240;
+
 const NavBar = () => {
     const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
-    const [drawerOpen, setDrawerOpen] = useState(false);
+    const [drawerOpen, setDrawerOpen] = useState(true);
 
     const handleDrawerToggle = () => {
         setDrawerOpen(!drawerOpen);
@@ -63,32 +64,80 @@ const NavBar = () => {
 
     return (
         <>
+            <AppBar
+                position="fixed"
+                sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, backgroundColor: '#b8dfe6' }}
+            >
+                <Toolbar
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                    }}
+                >
+                    <IconButton
+                        edge="start"
+                        color="gray"
+                        aria-label="menu"
+                        onClick={handleDrawerToggle}
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Avatar
+                        alt="Wagzters Logo"
+                        src="/wagzter-002.png"
+                        sx={{
+                            width: 56,
+                            height: 56,
+                        }}
+                    />
+                    <Box sx={{ width: '56px' }} /> {/* Add an empty box to balance the IconButton space */}
+                </Toolbar>
 
-            <Drawer variant="permanent" open={drawerOpen}>
-                <List>
-                    {drawerItems.map((item, index) => (
-                        <ListItem button key={index} component={RouterLink} to={item.route}>
-                            <ListItemIcon>{item.icon}</ListItemIcon>
-                            <ListItemText primary={item.text} />
-                        </ListItem>
-                    ))}
-                    {!isAuthenticated ? (
-                        <ListItem button onClick={loginWithRedirect}>
-                            <ListItemIcon>
-                                <LockOpenIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Login" />
-                        </ListItem>
-                    ) : (
-                        <ListItem button onClick={() => logout({ returnTo: window.location.origin })}>
-                            <ListItemIcon>
-                                <ExitToAppIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Logout" />
-                        </ListItem>
-                    )}
-                </List>
-            </Drawer>
+            </AppBar>
+            <Box component="nav" sx={{ width: { md: drawerWidth }, flexShrink: { md: 0 } }}>
+                <Drawer
+                    variant="persistent"
+                    open={drawerOpen}
+                    sx={{
+                        '& .MuiDrawer-paper': {
+                            boxSizing: 'border-box',
+                            width: drawerWidth,
+                            top: '64px', // AppBar height on desktop devices
+                            height: 'calc(100% - 64px)',
+                        },
+                    }}
+                >
+                    <List>
+                        {drawerItems.map((item, index) => (
+                            <ListItem button key={index} component={RouterLink} to={item.route}>
+                                <ListItemIcon>{item.icon}</ListItemIcon>
+                                <ListItemText primary={item.text} />
+                            </ListItem>
+                        ))}
+                        {!isAuthenticated ? (
+                            <ListItem button onClick={loginWithRedirect}>
+                                <ListItemIcon>
+                                    <LockOpenIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="Login" />
+                            </ListItem>
+                        ) : (
+                            <ListItem button onClick={() => logout({ returnTo: window.location.origin })}>
+                                <ListItemIcon>
+                                    <ExitToAppIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="Logout" />
+                            </ListItem>
+                        )}
+                    </List>
+                </Drawer>
+            </Box>
+            <Box component="main"
+                 sx={{ flexGrow: 1, p: 3, ml: { md: drawerWidth }, mt: { xs: '56px', sm: '64px' } }}
+            >
+                {/* your main content goes here */}
+            </Box>
         </>
     );
 };
