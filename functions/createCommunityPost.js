dc63e1a109a859ab774e50b55e8bdca21d2818e7
pmarcel-uw <pmarcel@uw.edu>
2023-06-05 20:35:20 -0700

    Implemented tagging for CreateCommunityPost.js

--- functions/createCommunityPost.js
+++ functions/createCommunityPost.js
@@ -12,7 +12,7 @@ exports.handler = async (event, context) => {
         return { statusCode: 405, body: 'Method Not Allowed' };
     }
 
-    const { ownerId, communityId, caption, fileUrl } = JSON.parse(event.body);
+    const { ownerId, communityId, caption, fileUrl, tags } = JSON.parse(event.body);
 
     const session = driver.session();
     try {
@@ -26,6 +26,30 @@ exports.handler = async (event, context) => {
         `, { ownerId, communityId, caption, fileUrl });
 
         const communityPost = result.records[0].get('post').properties;
+
+        // Handle tags, if any
+        if (tags) {
+            for (const tagName of tags) {
+                // Check if tag already exists, otherwise create a new one
+                const tagExists = await session.run(`
+                    MATCH (t:Tag {name: $tagName})
+                    RETURN t
+                `, { tagName });
+
+                if (!tagExists.records.length) {
+                    await session.run(`
+                        CREATE (t:Tag {name: $tagName})
+                    `, { tagName });
+                }
+
+                // Create a relationship from the post to the tag
+                await session.run(`
+                    MATCH (p:CommunityPost {id: $postId})
+                    MATCH (t:Tag {name: $tagName})
+                    MERGE (p)-[:TAGGED_AS]->(t)
+                `, { postId: communityPost.id, tagName });
+            }
+        }
 
         return { statusCode: 200, body: JSON.stringify(communityPost) };
     } catch (error) {
