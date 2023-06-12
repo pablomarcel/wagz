51ec256c3565508073aae294117b77523f2af432
pmarcel-uw <pmarcel@uw.edu>
2023-06-11 22:34:10 -0700

    attempt to implement stripe webhooks

--- src/components/StripePayments/BuyButtonComponent.js
+++ src/components/StripePayments/BuyButtonComponent.js
@@ -4,12 +4,15 @@ function BuyButtonComponent({ userEmail, publicFigureId }) {
     // Generating client-reference-id
     const clientReferenceId = `${userEmail}#${publicFigureId}`;
     console.log(`clientReferenceId: ${clientReferenceId}`)
+    console.log(`userEmail: ${userEmail}`)
+    console.log(`publicFigureId: ${publicFigureId}`)
 
     return (
         <stripe-buy-button
             buy-button-id="buy_btn_1NHsnXG3fcXidAci0anfxLbm"
             publishable-key="pk_test_Wgm8BdVa4LKI3f4Kay2wW1Fg00wnWcUWfy"
-            client-reference-id={clientReferenceId}
+            client-reference-id={publicFigureId}
+            customer-email={userEmail}
         >
         </stripe-buy-button>
     );
